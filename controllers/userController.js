21a2175f9b0d298173151129ada1aa27fdc30293
Trend20 <enockomondi305@gmail.com>
2024-09-13 23:26:17 +0300

    controller methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,19 @@ const User = require('../models/user');
 
 
 //create a user
+exports.createUser = async (req, res) => {
+
+}
 
 //get all existing users
+exports.getAllUsers = async (req, res) => {}
 
 //get a user by ID
+exports.getUserById = async (req, res) => {}
 
 //update a user
+exports.updateUser = async (req, res) => {}
+
 
 //delete a user
+exports.deleteUser = async (req, res) => {}
